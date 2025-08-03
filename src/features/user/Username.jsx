e3a17299d13db5be85ef106d57125016d4e224c8
ShahdRaafat <shahdraafat.725@gmail.com>
2025-08-04 02:08:19 +0300

    feat: add Redux for state management and integrate user functionality

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Shahoud</div>;
+  const username = useSelector((store) => store.user.username);
+  return (
+    <div className="hidden text-sm font-semibold tracking-widest md:block">
+      {username}
+    </div>
+  );
 }
 
 export default Username;
