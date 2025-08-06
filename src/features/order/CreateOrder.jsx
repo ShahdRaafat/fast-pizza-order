260445d317161338d971a7eca4a7931a56827f7c
ShahdRaafat <shahdraafat.725@gmail.com>
2025-08-06 20:08:11 +0300

    fix: adjust position of address loading button for better visibility

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -83,7 +83,7 @@ function CreateOrder() {
             )}
           </div>
           {!position.latitude && !position.longitude && (
-            <span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px]">
+            <span className="absolute right-[3px] top-[35px] z-50 md:right-[5px] md:top-[5px]">
               <Button
                 disabled={isLoadingAddress}
                 type="small"
